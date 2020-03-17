27c6c63cfd3cbff193e0041baf1ada229c037bd0
an pham <vincentpham1993@gmail.com>
2020-03-17 18:59:02 -0400

    update files

--- modules.sh
+++ modules.sh
@@ -3,6 +3,13 @@
 # Author: An Pham
 # Date: Sat Mar 14 11:12:19 EDT 2020
 
+root_check () {
+		if [ "$(id -u)" != "0" ]; then
+					echo "This script must be run as root or with sudo" 1>&2
+					exit 1
+		fi
+}
+
 image_checker () {
   if [ ! "${1#*.}" == ".tar.gz" ]; then
     echo "Error: can't find the image"
@@ -20,23 +27,24 @@ burn_sd () {
   echo "Image name: $2"
   echo "Disk name: $1"
   echo "Flash the $1 now"
+
   gunzip -c "$2" | sudo dd of="$1" status=progress ; ec="$?"
   [ "$ec" = 0 ] && echo "Flashing completed!" || exit 1
 }
 
 download_and_burn_image () {
+  root_check
   # download_and_burn_image '/dev/sdc' '124'
   echo "Downloading from http://dev.ole.org/treehouse-$2.img.gz"
   echo "Image name: treehouses-$2"
   echo "Disk name: $1"
   echo "Flashing the $1 now ... "
 
-  gunzip -c "$2" | sudo dd of="$1" status=progress ; ec="$?"
   curl "http://dev.ole.org/treehouse-$2.img.gz" | gunzip -c | sudo dd of="$1" status=progress ; ec="$?"
   [ "$ec" = 0 ] && echo "Done Flashing new image" || echo "Flashing failed" 
 
-  umount "$2" ; ec="$?" sync 
-  [ "$c" = 0 ] && echo "Successfully umounted" || echo "Error: fail to unmount the device"
+  umount "$2" ; ec="$?" ; sync 
+  [ "$ec" = 0 ] && echo "Successfully umounted" || echo "Error: fail to unmount the device"
 }
 
 save_image () {
@@ -44,8 +52,8 @@ save_image () {
   local image_name="$1" 
   local block_device="$2"
 
-  dd if="$block_device" of="$image_name" bs=4M status=progress  ; ec="$?"
-  [ "$ec" = 0 ]  && echo "Image is backed up" || echo "Error: fail to back up the image"
+  dd if="$block_device" of="$image_name" | gzip -c > "$image_name.img.gz" ; ec="$?"
+  [ "$ec" = 0 ]  && echo "Sucess: image is backed up" || echo "Error: fail to back up the image"
 }
 
 images_management () {
@@ -55,12 +63,10 @@ images_management () {
 		echo '  -h,--help    show the help page'
   echo '  -l,--list    show current saved images'
 		echo '  -c,--clean   clean the disk drive' 
-		echo '  -d,--destroy destroy the vm'
   echo "  -b,--burn    burn an image"
   echo "Example: "
   echo "  $(basename $0) -b /dev/sda treehouses.gz"
   echo ''
-  echo "  --down-burn  download the latest image and burn it to a sd card"
   echo ""
 }
 
